5de038a663c324d1e97cf7290c1c1293a09664de
romankalab <roan.kalabgmail.com>
2021-03-21 20:30:42 +0100

    CompanyService

---
+++ spring-boot-app/spring-boot-app/src/main/java/com/appslab/springbootapp/CompanyRepository.java
@@ -0,0 +1,7 @@
+package com.appslab.springbootapp;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
+
+}
---
+++ spring-boot-app/spring-boot-app/src/main/java/com/appslab/springbootapp/EntityManaging.java
@@ -0,0 +1,11 @@
+package com.appslab.springbootapp;
+
+import javax.persistence.EntityManagerFactory;
+import java.util.logging.Logger;
+
+public class EntityManaging {
+    Logger log = Logger.getLogger(this.getClass().getName());
+
+    private EntityManagerFactory emf;
+
+}
